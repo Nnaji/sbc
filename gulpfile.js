e2218e589b7bb90ebcbe109be19acd1611850c34
Kingsley <kingsley.nnaji@gmail.com>
2017-10-25 23:16:08 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp'),
+	browserSync = require('browser-sync').create(),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename'),
+	sourceMaps = require('gulp-sourcemaps'),
+	handlebars = require('gulp-compile-handlebars'),
+	minifyCss = require('gulp-minify-css');
+
+gulp.task('styles', function () {
+	gulp.src(['src/styles/**/*.css'])
+		.pipe(sourceMaps.init())
+		.pipe(rename('main.min.css'))
+		.pipe(minifyCss())
+		.pipe(sourceMaps.write())
+		.pipe(gulp.dest('dist/styles'))
+		.pipe(browserSync.stream());
+	console.log('CSS file or files minified');
+});
+
+gulp.task('scripts', function () {
+	gulp.src(['src/scripts/**/*.js'])
+		.pipe(sourceMaps.init())
+		.pipe(rename('main.min.js'))
+		.pipe(uglify())
+		.pipe(sourceMaps.write())
+		.pipe(gulp.dest('dist/scripts'))
+		.pipe(browserSync.stream());
+	console.log('JavaScript file or files uglified');
+});
+
+gulp.task('default', function () {
+	console.log('This task is running');
+	browserSync.init({
+		server: './'
+	});
+
+	gulp.watch(['src/**/*', '*.html'], browserSync.reload);
+	gulp.watch('src/scripts/**/*.js', ['scripts']);
+	gulp.watch('src/styles/**/*.css', ['styles']);
+
+	//	gulp.watch('*.html', browserSync.)
+});
